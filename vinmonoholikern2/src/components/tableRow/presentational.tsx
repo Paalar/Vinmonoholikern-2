0dfba4eb3e604534895d84fa3e3152680a0a31b2
Paalar <paaledward@gmail.com>
2019-08-10 00:58:36 +0200

    Make table items not move

--- vinmonoholikern2/src/components/tableRow/presentational.tsx
+++ vinmonoholikern2/src/components/tableRow/presentational.tsx
@@ -7,15 +7,15 @@ type IProps = ITableItems;
 const TableRow: FunctionComponent<IProps> = (props) => {
   const body = props.items.map((item) =>
     <tr className="item-table-row" key={item.number}>
-      <td className="td-left">
+      <td className="td-left item-name">
         <a className="item-link" href={item.url} target="_blank" >{item.name}</a>
       </td>
-      <td className="td-left">{item.type}</td>
-      <td>{item.apk}</td>
-      <td>Kr. {item.price}</td>
-      <td>{item.abv}%</td>
-      <td>{item.volume}L</td>
-      <td className="td-left">{item.range}</td>
+      <td className="td-left item-type">{item.type}</td>
+      <td className="item-kpa">{item.apk}</td>
+      <td className="item-price">Kr. {item.price}</td>
+      <td className="item-abv">{item.abv}%</td>
+      <td className="item-volume">{item.volume}L</td>
+      <td className="td-left item-range">{item.range}</td>
     </tr>);
   return (
     <tbody id="item-table-body">
