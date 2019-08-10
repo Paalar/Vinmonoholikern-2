6179684e06327c864251fce398709003be6c9167
Paalar <paaledward@gmail.com>
2019-08-10 18:37:59 +0200

    Create footer

---
+++ vinmonoholikern2/src/components/footer/presentational.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from "react";
+import "./footer.scss";
+
+const Footer: FunctionComponent = () => {
+  const csvLink =
+    <a href="https://www.vinmonopolet.no/datadeling/csv">
+      https://www.vinmonopolet.no/datadeling/csv
+    </a>
+
+  return (
+    <div id="footer">
+      <div id="footer-seperator"></div>
+      <div id="footer-info">
+        <p>
+          <span className="text-important">Vinmonopolet har ingen tilknytting
+          til Vinmonoholikern</span> utenom at dataen blir hentet fra
+          Vinmonopolets CSV filer. Disse filene kan bli hentet fra {csvLink}
+        </p>
+        <p>
+          Alle linkene går til Vinmonopolet.no og <span className="text-important">
+          all bestilling, kjøp, og utlevering</span> blir utført av Vinmonopolet
+          eller av Vinmonopolets samarbeidspartnere.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
